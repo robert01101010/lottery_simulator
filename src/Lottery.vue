cb94115543772e79ff21eb0f81c5d4c7a4c54c4a
Robert Jablonski <Robert.Jablonski@smart4aviation.aero>
2018-02-26 21:48:08 +0100

    created main view

--- src/Lottery.vue
+++ src/Lottery.vue
@@ -5,14 +5,19 @@
             <div class="coupon-generator__buttons">
                 <span class="coupon-generator__button-item"
                       v-for="index in couponsMaxAmount"
-                      @click="generateCoupon(index)">
+                      @click="generateCoupons(index)">
                     {{index}}
                 </span>
             </div>
         </div>
         <div class="coupons-generated" v-if="couponsToGenerate">
-            <div class="coupon-item" v-for="index in couponsToGenerate">
-                asda
+            <div class="coupon-item-container"
+                 v-for="index in couponsToGenerate">
+                <div class="coupon-item">
+                    Coupon number: {{index}}
+                    <div class="coupon-item__title">Generated numbers:</div>
+                    <div class="coupon-item__numbers">1,2,3,3,4,4</div>
+                </div>
             </div>
         </div>
     </div>
@@ -26,12 +31,33 @@
     data () {
       return {
         couponsMaxAmount: 8,
-        couponsToGenerate: 0
+        couponsToGenerate: 0,
+        coupons: [],
+        lotteryRange: 49,
+        createdNumberCount: 6
       }
     },
+    computed: {},
     methods: {
-      generateCoupon (amount) {
+      generateCoupons (amount) {
         this.couponsToGenerate = amount
+        this.generateNumbers()
+      },
+
+      generateNumbers () {
+        for (let i = 0; i < this.couponsToGenerate; i += 1) {
+          this.generateNumbersForCoupon(i)
+        }
+      },
+
+      generateNumbersForCoupon (idx) {
+        const numbers = []
+
+        for (let i = 0; i < this.createdNumberCount; i += 1) {
+          const number = Math.round(Math.random() * (this.lotteryRange - 1) + 1);
+          numbers.push(number)
+          // different number
+        }
       }
     },
     mounted () {
@@ -42,48 +68,62 @@
 
 <style lang="less" scoped>
     @standard-button-height: 50px;
+    @double-standard-button-height: (@standard-button-height + @standard-button-height);
     @standard-border: 1px solid black;
-.lottery-container{
-    margin: 0 auto;
-    width: 60%;
 
-    .coupon-generator-container {
-        display: flex;
-        flex-direction: column;
-        background: gold;
+    .lottery-container {
+        margin: 0 auto;
+        width: 60%;
 
-        .coupon-generator__question {
-            height: @standard-button-height;
-            line-height: @standard-button-height;
-            width: 100%;
-            text-align: center;
+        .coupon-generator-container {
+            display: flex;
+            flex-direction: column;
+            background: gold;
+
+            .coupon-generator__question {
+                height: @standard-button-height;
+                line-height: @standard-button-height;
+                width: 100%;
+                text-align: center;
+            }
+
+            .coupon-generator__buttons {
+                height: @standard-button-height;
+                line-height: @standard-button-height;
+                display: flex;
+                flex-direction: row;
+
+                .coupon-generator__button-item {
+                    text-align: center;
+                    border: @standard-border;
+                    width: 12.5%;
+
+                    &:hover {
+                        cursor: pointer;
+                        background: lighten(gold, 10%);
+                    }
+                }
+
+            }
         }
 
-        .coupon-generator__buttons {
-            height: @standard-button-height;
-            line-height: @standard-button-height;
+        .coupons-generated {
             display: flex;
-            flex-direction: row;
+            flex-wrap: wrap;
+            background: gold;
 
-            .coupon-generator__button-item {
-                text-align: center;
-                border: @standard-border;
-                width: 12.5%;
+            .coupon-item-container {
+                width: calc(25% - 20px);
+                height: @double-standard-button-height;
+                padding: 10px;
 
-                &:hover {
-                    cursor: pointer;
-                    background: lighten(gold, 10%);
+                .coupon-item {
+                    background: goldenrod;
+
                 }
             }
 
         }
-    }
-
-    .coupons-generated {
-        background: gold;
-
 
     }
-
-}
 </style>
